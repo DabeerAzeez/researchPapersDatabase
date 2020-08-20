dd454c5c09ffbf3463f35afca5e61c5bb6a07a6c
Dabeer Abdul-Azeez <59609232+GuyInFridge@users.noreply.github.com>
2020-08-20 09:03:49 -0400

    Add and use DATASET constant

--- Equipment.js
+++ Equipment.js
@@ -1,3 +1,4 @@
+const DATASET = "#EquipmentDS"
 $w.onReady(function () {
 	// Show 'image coming soon' if image unavailable
 	$w("#EquipmentRepeater").onItemReady((($item, itemData, index) => {
@@ -20,11 +21,23 @@ $w.onReady(function () {
 /**
  * Check to see if all data from available results is being shown and toggle loading button appropriately with an
  * alternative container
+ * Manually load another page of data for the dataset and display a loading GIF as necessary
+ * @param {click event} event - click event for loadMoreButton
+ */
+export async function loadMoreButton_click(event) {
+	$w("#loadingGIFmore").show();
+	await $w(DATASET).loadMore();
+  $w("#loadingGIFmore").hide();
+  
+  let total = $w(DATASET).getTotalCount();
+
+	updateEndContainer(total);
+}
  */
 function updateEndContainer() {
 	let total = $w("#EquipmentDS").getTotalCount();
 	let nonZeroItems = total > 0;
-	let allPagesLoaded = $w('#EquipmentDS').getCurrentPageIndex() === $w('#EquipmentDS').getTotalPageCount();
+	let allPagesLoaded = $w(DATASET).getCurrentPageIndex() === $w(DATASET).getTotalPageCount();
 
 	// Show loading button only if there are more pages of papers to load
 	if (nonZeroItems && !allPagesLoaded) {
@@ -34,16 +47,4 @@ function updateEndContainer() {
 		$w("#loadMoreButton").collapse(); 
 		$w("#noLoadingButtons").expand();
 	}
-}
-
-/**
- * Manually load another page of data for the dataset and display a loading GIF as necessary
- * @param {click event} event - click event for loadMoreButton
- */
-export async function loadMoreButton_click(event) {
-	$w("#loadingGIFmore").show();
-	await $w("#EquipmentDS").loadMore();
-	$w("#loadingGIFmore").hide();
-
-	updateEndContainer();
 }
