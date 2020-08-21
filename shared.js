c6bf531c11fca5bf4e8cfad002eecb8dfab05c55
Dabeer Abdul-Azeez <59609232+GuyInFridge@users.noreply.github.com>
2020-08-21 08:29:29 -0400

    Updated shared.js comments

--- shared.js
+++ shared.js
@@ -1,5 +1,5 @@
 /**
- * Check an item with a _.id (e.g. a repeater item being read from a database) for a set of required properties
+ * Check an item with a ._id property (e.g. a repeater item being read from a database) for a set of required properties
  * @param {object} itemData - object to be checked for all required properties
  * @param {array [string]} requiredProperties - array of all properties that are required
  */
