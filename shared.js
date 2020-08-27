1e56118a90b73287fd5c3cbf5f085feff53df654
Dabeer Abdul-Azeez <59609232+GuyInFridge@users.noreply.github.com>
2020-08-26 21:48:18 -0400

    Add @throw documentation to shared.js

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@
  * Check an item with a ._id property (e.g. a repeater item being read from a database) for a set of required properties
  * @param {object} itemData - object to be checked for all required properties
  * @param {array [string]} requiredProperties - array of all properties that are required
+ * @throws {Error} if item is passed without an ._id property
+ * @throws {Error} if the item is missing any of the required properties
  */
 export function checkItemProperties(item, requiredProperties) {
   if (item._id === undefined) {
