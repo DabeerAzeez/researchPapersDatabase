a6b0341167d4c3075d31a141eea822271b94a3e6
Dabeer Abdul-Azeez <59609232+GuyInFridge@users.noreply.github.com>
2020-08-26 21:52:48 -0400

    Update filterResearchPapers.py documentation

--- filterResearchPapers/filterResearchPapers.py
+++ filterResearchPapers/filterResearchPapers.py
@@ -1,3 +1,8 @@
+"""
+A python file I used to extract research paper information from a text file and compile it into a .csv file (for
+importing into the Wix database).
+"""
+
 import codecs
 import csv
 
@@ -5,7 +10,7 @@ with codecs.open('ResearchPapers.txt', encoding='utf-8', mode='r') as f:
     content = f.readlines()
 
 with codecs.open('researchPapers.csv', encoding='utf-8', mode='w+') as research_csv:
-    for i in range(0, len(content)-1, 3):  # Working in line groups of 3
+    for i in range(0, len(content)-1, 3):  # Working in groups of 3 lines of the text file at a time
         title = content[i].strip()
         citation = content[i+1].strip()
 
